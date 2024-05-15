ec092e95fa5ed2626d78d2b94f8e6b10120ed015
mts755Dev <muhammadtalhasaleem.755@gmail.com>
2024-05-15 01:49:23 +0100

    fix: linting error removed

--- components/infoCard/infoCard.tsx
+++ components/infoCard/infoCard.tsx
@@ -27,7 +27,7 @@ export function InfoCard() {
         <Card className="w-full border-0 md:border md:border-[2px] flex-col items-center justify-center mb-10">
             <div className="flex items-center justify-between">
                 <CardTitle className="text-base mt-5 mb-5 text-[#212B36] md:mx-5">
-                    You will be chatting with {type} to discuss your incident. Please click "Next" to continue to the chat page.
+                    You will be chatting with {type} to discuss your incident. Please click &quot;Next&quot; to continue to the chat page.
                 </CardTitle>
             </div>
             <Button className="mt-5 mx-5 mb-5" variant="outline" onClick={nextButtonHandler}>Next</Button>
